fa3466a6a7e899b6c823b4be061e5024d9574f0c
unknown <arthurluiz.developer@gmail.com>
2026-04-29 15:09:25 +0100

    Player movement fixed.

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -1,67 +1,106 @@
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
-[RequireComponent(typeof(PlayerInput))]
-[RequireComponent(typeof(Character))]
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement")]
-    [SerializeField] private float moveSpeed;
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 6f;
     [SerializeField] private float sprintMultiplier;
+    [SerializeField] private float rotationSpeed = 15f;
+
+    [Header("References")]
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private LayerMask groundLayer;
 
     private CharacterController controller;
-    private PlayerInput input;
     private Character character;
+    private Animator animator;
+    private PlayerInput input;
 
-    private void Start()
+
+    void Start()
     {
+        animator = GetComponent<Animator>();
+        character = GetComponent<Character>();
         controller = GetComponent<CharacterController>();
         input = GetComponent<PlayerInput>();
-        character = GetComponent<Character>();
-        Debug.Log(input.moveInput);
+
+        if (cameraTransform == null)
+            cameraTransform = Camera.main.transform;
     }
 
-    private void Update()
+    void Update()
     {
         if (!CanMove())
+        {
+            animator.SetFloat("VelocityX", 0f, 0.1f, Time.deltaTime);
+            animator.SetFloat("VelocityZ", 0f, 0.1f, Time.deltaTime);
+            animator.SetFloat("MoveMagnitude", 0f);
             return;
+        }
 
+        HandleRotationToMouse();
         HandleMovement();
     }
 
-    private void HandleMovement()
+    void HandleRotationToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 pointToLook = ray.GetPoint(rayDistance);
+
+            Vector3 lookDirection = new Vector3(pointToLook.x, transform.position.y, pointToLook.z) - transform.position;
+
+            if (lookDirection != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    void HandleMovement()
     {
         Vector2 moveInput = input.moveInput;
 
-        Vector3 move = new Vector3(
-            moveInput.x,
-            0f,
-            moveInput.y
-        );
-        move = new Vector3(
-            move.x - move.z,
-            0f,
-            move.x + move.z
-        );
+        Vector3 camForward = cameraTransform.forward;
+        Vector3 camRight = cameraTransform.right;
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
 
-        move = move.normalized;
+        Vector3 moveDirection = (camForward * moveInput.y + camRight * moveInput.x).normalized;
 
         float speed = moveSpeed;
 
-        if (input.sprintHeld && move != Vector3.zero)
+        if (input.sprintHeld && moveInput != Vector2.zero)
         {
             speed = moveSpeed * sprintMultiplier;
         }
 
-        controller.Move(move * speed * Time.deltaTime);
+        if (moveDirection.magnitude >= 0.1f)
+        {
+            controller.Move(moveDirection * speed * Time.deltaTime);
+        }
+
+        Vector3 localMove = transform.InverseTransformDirection(moveDirection);
+
+        animator.SetFloat("VelocityX", localMove.x, 0.1f, Time.deltaTime);
+        animator.SetFloat("VelocityZ", localMove.z, 0.1f, Time.deltaTime);
+        animator.SetFloat("MoveMagnitude", moveDirection.magnitude);
     }
 
     private bool CanMove()
     {
-        if (character.CharacterState == Character.State.Attacking)
+        if (character.CharacterState == Character.State.Attacking ||
+            character.CharacterState == Character.State.Lunging)
             return false;
 
         return true;
     }
-
 }
