a9ba2590abde9380018719b06c794d14e5f90f40
Frederico-Carvalho <frederico.carvalho@gmail.com>
2026-04-30 21:12:24 +0100

    Updated the RotationMouse Script

--- Assets/Scripts/Player/RotateToFaceMouse.cs
+++ Assets/Scripts/Player/RotateToFaceMouse.cs
@@ -1,41 +1,36 @@
 using UnityEngine;
 using UnityEngine.UI;
-
 public class RotateToFaceMouse : MonoBehaviour
 {
+    [Header("Settings")]
+    [SerializeField] private float minDistance = 1f;
+    [SerializeField] private float rotationSpeed = 10f;
+
+
     public Camera mainCamera;
     public LayerMask groundLayer;
     public RawImage renderImage;
-
-    // Update is called once per frame
     void Update()
     {
         Vector2 localPoint;
-
         RectTransform rectTransform = renderImage.rectTransform;
-
         if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, Input.mousePosition, null, out localPoint))
         {
-            // Convert to UV (0 to 1)
             Vector2 uv = new Vector2(
                 (localPoint.x + rectTransform.rect.width * 0.5f) / rectTransform.rect.width,
                 (localPoint.y + rectTransform.rect.height * 0.5f) / rectTransform.rect.height
             );
-
             Ray ray = mainCamera.ViewportPointToRay(uv);
-
             if (Physics.Raycast(ray, out RaycastHit hit, 100f, groundLayer))
             {
                 Vector3 direction = hit.point - transform.position;
                 direction.y = 0;
-
-                if (direction != Vector3.zero)
+                if (direction.magnitude < minDistance) return;
                 {
                     Quaternion lookRotation = Quaternion.LookRotation(direction);
-                    transform.rotation = Quaternion.Euler(0f, lookRotation.eulerAngles.y, 0f);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
                 }
             }
-            //Debug.Log(uv);
         }
     }
 }
