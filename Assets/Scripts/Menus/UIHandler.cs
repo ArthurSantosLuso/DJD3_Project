939ca826b41547bbd4132d036e63acce37bb1278
Catiamn <catia.nascimento2006@gmail.com>
2026-05-03 17:07:58 +0100

    Fixed the stamina bar so all the numbers are accurate

--- Assets/Scripts/Menus/UIHandler.cs
+++ Assets/Scripts/Menus/UIHandler.cs
@@ -16,7 +16,22 @@ public class UIHandler : MonoBehaviour
 
     public void SetBarValue(int barIdx, float currentValue, float maxValue)
     {
-        bars[barIdx].fillAmount = currentValue / maxValue;
+        // Get normal values 0.0 - 1-0
+        float percentage = currentValue / maxValue;
+
+        
+        if (barIdx == 1) //  Stamina needs to be the second bar in the list
+        {
+            // change those value to 0.2 - 0.8 so it makes sense with the stamina bar sprite
+            float mappedValue = Mathf.Lerp(0.2f, 0.8f, percentage);
+
+            bars[barIdx].fillAmount = mappedValue;
+        }
+        else
+        {
+            
+            bars[barIdx].fillAmount = percentage;
+        }
     }
 
     public void ShowDeathScreen()
