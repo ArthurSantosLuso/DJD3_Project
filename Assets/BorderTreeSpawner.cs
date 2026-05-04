51b313d0acb926680ac19c788e912d7e7e694d27
unknown <arthurluiz.developer@gmail.com>
2026-05-04 15:14:43 +0100

    Border generator now accepts entrances.

--- Assets/BorderTreeSpawner.cs
+++ Assets/BorderTreeSpawner.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BorderTreeSpawner : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject[] treePrefabs;
-    [SerializeField]
-    private float roomWidth = 20f, roomDepth = 20f;
-    [SerializeField]
-    private float spacing = 2f;
-    [SerializeField] 
-    private int extraLayers = 2;
+    [SerializeField] private GameObject[] treePrefabs;
+    [SerializeField] private float roomWidth = 20f, roomDepth = 20f;
+    [SerializeField] private float spacing = 2f;
+    [SerializeField] private int extraLayers = 2;
+
+    [Header("Entrances")]
+    [SerializeField] private bool entranceNorth = false;
+    [SerializeField] private bool entranceSouth = false;
+    [SerializeField] private bool entranceEast = false;
+    [SerializeField] private bool entranceWest = false;
+    [SerializeField] private float entranceWidth = 4f;
 
     void Start() => SpawnBorderTrees();
 
@@ -22,15 +26,23 @@ public class BorderTreeSpawner : MonoBehaviour
 
             for (float x = -ex; x <= ex; x += spacing)
             {
-                SpawnTree(new Vector3(x, 0, ez));
-                SpawnTree(new Vector3(x, 0, -ez));
+                if (!IsInEntrance(x, entranceNorth)) SpawnTree(new Vector3(x, 0, ez));
+                if (!IsInEntrance(x, entranceSouth)) SpawnTree(new Vector3(x, 0, -ez));
             }
+
             for (float z = -ez + spacing; z < ez; z += spacing)
             {
-                SpawnTree(new Vector3(ex, 0, z));
-                SpawnTree(new Vector3(-ex, 0, z));
+                if (!IsInEntrance(z, entranceEast)) SpawnTree(new Vector3(ex, 0, z));
+                if (!IsInEntrance(z, entranceWest)) SpawnTree(new Vector3(-ex, 0, z));
             }
         }
+    }
+
+    bool IsInEntrance(float pos, bool hasEntrance)
+    {
+        if (!hasEntrance) return false;
+        float half = entranceWidth / 2f;
+        return pos >= -half && pos <= half;
     }
 
     void SpawnTree(Vector3 pos)
@@ -38,6 +50,24 @@ public class BorderTreeSpawner : MonoBehaviour
         pos += new Vector3(Random.Range(-0.3f, 0.3f), 0, Random.Range(-0.3f, 0.3f));
         var prefab = treePrefabs[Random.Range(0, treePrefabs.Length)];
         var t = Instantiate(prefab, pos, Quaternion.identity);
+        t.transform.rotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
         t.transform.localScale *= Random.Range(0.85f, 1.15f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        float halfW = roomWidth / 2f;
+        float halfD = roomDepth / 2f;
+        float halfE = entranceWidth / 2f;
+
+        Gizmos.color = Color.green;
+
+        if (entranceNorth) Gizmos.DrawLine(new Vector3(-halfE, 0, halfD), new Vector3(halfE, 0, halfD));
+        if (entranceSouth) Gizmos.DrawLine(new Vector3(-halfE, 0, -halfD), new Vector3(halfE, 0, -halfD));
+        if (entranceEast) Gizmos.DrawLine(new Vector3(halfW, 0, -halfE), new Vector3(halfW, 0, halfE));
+        if (entranceWest) Gizmos.DrawLine(new Vector3(-halfW, 0, -halfE), new Vector3(-halfW, 0, halfE));
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(roomWidth, 1f, roomDepth));
     }
 }
