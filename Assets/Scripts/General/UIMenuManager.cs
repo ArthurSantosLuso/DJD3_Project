de648e38bfe3091a4d4ecf06b1109e9695e66985
Frederico-Carvalho <frederico.carvalho@gmail.com>
2026-05-26 02:07:50 +0100

    3D Main Menu Base Done

--- Assets/Scripts/General/UIMenuManager.cs
+++ Assets/Scripts/General/UIMenuManager.cs
@@ -3,38 +3,73 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Unity.Cinemachine;
+using UnityEngine.UI;
 
 public class UIMenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject gameplayUI;
+    [Header("UI")]
     [SerializeField] private GameObject menuUI;
-    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private GameObject healthBar;
+    [SerializeField] private GameObject weaponDisplay;
+    [SerializeField] private GameObject staminaBar;
+    [SerializeField] private GraphicRaycaster gameplayRaycaster;
+    [SerializeField] private GameObject renderingCanvas;
 
+    [Header("Input")]
+    [SerializeField] private GameObject playerObject;
+
+    [Header("Cameras")]
     public CinemachineCamera currentCamera;
     [SerializeField] private CinemachineCamera menuCamera;
     [SerializeField] private CinemachineCamera gameplayCamera;
+    [SerializeField] private CinemachineBrain cinemachineBrain;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private CinemachineCamera optionsCamera;
+    [SerializeField] private RenderTexture pixelRenderTexture;
 
-    void Start()
+    void Awake()
     {
-        gameplayUI.SetActive(false);
+        // Cameras
+        menuCamera.Priority = 100;
+        gameplayCamera.Priority = 0;
+        optionsCamera.Priority = 0;
+        currentCamera = menuCamera;
+        cinemachineBrain.LensModeOverride.Enabled = false;
+        mainCamera.targetTexture = null; 
+
+        // UI
         menuUI.SetActive(true);
-        playerInput.enabled = false;
-        currentCamera.Priority++;
+        healthBar.SetActive(false);
+        weaponDisplay.SetActive(false);
+        staminaBar.SetActive(false);
+        gameplayRaycaster.enabled = false;
+        playerObject.SetActive(false);
+        renderingCanvas.SetActive(false);
     }
 
     public void UpdateCamera(CinemachineCamera target)
     {
-        currentCamera.Priority--;
+        currentCamera.Priority = 0;
         currentCamera = target;
-        currentCamera.Priority++;
+        currentCamera.Priority = 100;
+
     }
 
     public void StartGame()
     {
-        gameplayUI.SetActive(true);
+        // Cameras
+        currentCamera.Priority = 0;
+        gameplayCamera.Priority = 100;
+        cinemachineBrain.LensModeOverride.Enabled = true;
+        mainCamera.targetTexture = pixelRenderTexture; 
+
+        // UI
         menuUI.SetActive(false);
-        playerInput.enabled = true;
-        menuCamera.Priority = 0;
-        gameplayCamera.Priority = 10;
+        healthBar.SetActive(true);
+        weaponDisplay.SetActive(true);
+        staminaBar.SetActive(true);
+        gameplayRaycaster.enabled = true;
+        playerObject.SetActive(true);
+        renderingCanvas.SetActive(true);
     }
 }
