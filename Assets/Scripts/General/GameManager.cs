d652a4cb96216f1fb83b777347af40812fc3bffc
unknown <arthurluiz.developer@gmail.com>
2026-06-10 00:49:09 +0100

    Save system.

--- Assets/Scripts/General/GameManager.cs
+++ Assets/Scripts/General/GameManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine.SceneManagement;
 
 /*
 This script handles:
@@ -35,13 +37,21 @@ public class GameManager : MonoBehaviour
     {
         _instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadGame(allUpgrades);
     }
     #endregion
 
+    private void OnEnable()
+    {
+    }
+
     [SerializeField] private int teddyBearCount;
     [SerializeField] private int availableUpgrades;
+    [SerializeField] private List<UpgradeData> allUpgrades;
 
     private List<UpgradeData> playerUpgrades = new();
+
+    private const string SAVE_KEY = "SaveData";
 
     public List<UpgradeData> PlayerUpgrades => playerUpgrades;
     public int TeddyBearCount => teddyBearCount;
@@ -61,16 +71,44 @@ public class GameManager : MonoBehaviour
 
     public void SaveGame()
     {
+        SaveData save = new SaveData
+        {
+            teddyBearCount = teddyBearCount,
+            availableUpgrades = availableUpgrades,
+            playerUpgradesIDs = playerUpgrades.ConvertAll(s => s.ID)
+        
+        };
 
+        string json = JsonUtility.ToJson(save);
+        PlayerPrefs.SetString(SAVE_KEY, json);
+        PlayerPrefs.Save();
     }
 
-    #region Game State - Pause
+    public void LoadGame(List<UpgradeData> allUpgrades)
+    {
+        if (!PlayerPrefs.HasKey(SAVE_KEY)) return;
 
-    /// <summary>
-    /// Disables all player controlled components and pauses the game.
-    /// Look for game object via FindGameObjectWithTag("Player").
-    /// </summary>
-    public void StopPlayerActions()
+        string json = PlayerPrefs.GetString(SAVE_KEY);
+        SaveData save = JsonUtility.FromJson<SaveData>(json);
+
+        teddyBearCount = save.teddyBearCount;
+        availableUpgrades = save.availableUpgrades;
+
+        playerUpgrades.Clear();
+        foreach (string id in save.playerUpgradesIDs)
+        {
+            UpgradeData match = allUpgrades.Find(s => s.ID == id);
+            if (match != null) playerUpgrades.Add(match);
+        }
+    }
+
+#region Game State - Pause
+
+/// <summary>
+/// Disables all player controlled components and pauses the game.
+/// Look for game object via FindGameObjectWithTag("Player").
+/// </summary>
+public void StopPlayerActions()
     {
         CanPlayerAct = false;
         GameObject player = GameObject.FindGameObjectWithTag("Player");
--- Assets/Scripts/Upgrades/UpgradeData.cs
+++ Assets/Scripts/Upgrades/UpgradeData.cs
@@ -6,10 +6,12 @@ public class UpgradeData : ScriptableObject
 {
     public enum UpgradeTypes { Life, ShotgunDamage, AxeDamage, AxeAbilityCooldown, ShotgunAbilityCooldown, AxeAbilityDamage, ShotgunAbilityDamage }
 
+    [SerializeField] private string         id; 
     [SerializeField] private string         upgradeName;
     [SerializeField] private int            amountToChange;
     [SerializeField] private UpgradeTypes   upgradeType;
 
+    public string ID => id;
     public string UpgradeName => upgradeName;
     public int AmountToChange => amountToChange;
     public UpgradeTypes UpgradeType => upgradeType;
