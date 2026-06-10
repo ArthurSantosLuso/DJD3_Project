631f417427da50945c175200a0805344eb84802a
unknown <arthurluiz.developer@gmail.com>
2026-06-10 17:18:26 +0100

    some quality changes to main hall scenes. New level start confirmation added when have upgrades avaliable.

--- Assets/Scripts/Utils/LevelTrigger.cs
+++ Assets/Scripts/Utils/LevelTrigger.cs
@@ -6,13 +6,28 @@ public class LevelTrigger : MonoBehaviour
 {
     [SerializeField, SceneDropdown] private string  sceneToOpen;
     [SerializeField] private ScreenFader screenFader;
+    [SerializeField] private GameObject confirmationPanel;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerMovement>() != null)
         {
+            if (GameManager.Instance.HasUpgradeAvaliable)
+            {
+                confirmationPanel.SetActive(true);
+                GameManager.Instance.StopPlayerActions();
+                return;
+            }
+            
             screenFader.FadeAndLoad(sceneToOpen, 1.5f);
 
         }
+    }
+
+    public void OpenLevel()
+    {
+        confirmationPanel.SetActive(false);
+        GameManager.Instance.ActivatePlayerActions();
+        screenFader.FadeAndLoad(sceneToOpen, 1.5f);
     }
 }
